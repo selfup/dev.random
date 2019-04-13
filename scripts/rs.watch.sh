35edb4ab817dd2fce1b8ab869a5757a66d3ef88d
Regis <boudinot.regis@yahoo.com>
2019-04-13 11:19:28 -0500

    release rust mode

--- scripts/rs.watch.sh
+++ scripts/rs.watch.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-echo 'main.rs' | entr -rs 'rustc main.rs && ./main'
+# pass -r to the watch script for release mode
+
+if [[ $1 == "-r" ]]
+then
+  echo 'main.rs' | entr -rs 'rustc -C debuginfo=0 -C opt-level=3 main.rs && ./main'
+else
+  echo 'main.rs' | entr -rs 'rustc main.rs && ./main'
+fi
